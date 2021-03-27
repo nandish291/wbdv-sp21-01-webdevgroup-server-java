ed64568661dd0d4e55fc048fa8a21613424e030c
Nandish Murugeshi <nandish291@live.com>
2021-03-27 19:46:40 -0400

    project structure

---
+++ src/main/java/com/webdevgroup/sp2101webdevegroupserverjava/Sp2101webdevegroupserverjavaApplication.java
@@ -0,0 +1,13 @@
+package com.webdevgroup.sp2101webdevegroupserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp2101webdevegroupserverjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp2101webdevegroupserverjavaApplication.class, args);
+    }
+
+}

54a7507e1a58b93d02b0f0dad29f42bf64fd5db8
Nandish Murugeshi <nandish291@live.com>
2021-04-22 03:12:19 -0400

    Redis integration for session management on heroku

--- src/main/java/com/webdevgroup/sp2101webdevegroupserverjava/config/AppConfig.java
+++ src/main/java/com/webdevgroup/sp2101webdevegroupserverjava/config/AppConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class RedisConfig {
+public class AppConfig {
 
     @Bean
     public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {

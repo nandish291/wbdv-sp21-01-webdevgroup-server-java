e942120fe4d8b50a34100860c71c36b62635e240
Nandish Murugeshi <nandish291@live.com>
2021-05-06 14:01:57 -0400

    Samesite test

--- src/main/java/com/webdevgroup/sp2101webdevegroupserverjava/Sp2101webdevegroupserverjavaApplication.java
+++ src/main/java/com/webdevgroup/sp2101webdevegroupserverjava/Sp2101webdevegroupserverjavaApplication.java
@@ -3,6 +3,9 @@ package com.webdevgroup.sp2101webdevegroupserverjava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -10,6 +13,13 @@ public class Sp2101webdevegroupserverjavaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Sp2101webdevegroupserverjavaApplication.class, args);
+    }
+
+    @Bean
+    public CookieSerializer cookieSerializer(){
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setSameSite("None");
+        return serializer;
     }
 
 
